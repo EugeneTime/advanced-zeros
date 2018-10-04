8fd512b5e38509156b7a6db0dc7f93da2d634a26
EugeneTime <eugenetime@bk.ru>
2018-10-04 14:46:17 +0300

    EugeneTime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let result = number;
+  let baseSystem = base;
+
+  for (let i = 2; i <= baseSystem; i++) {
+
+    let multipliers = 0;
+    while (baseSystem % i === 0) {
+      baseSystem = Math.floor(baseSystem / i); 
+      multipliers++;
+    }
+
+    let zeros = 0;
+    let count = number;
+    while (count / i > 0) {
+      zeros += Math.floor(count / i); 
+      count = Math.floor(count / i);
+    }
+
+    result = Math.floor(Math.min(result, zeros / multipliers));
+  }
+
+  return result;
 }
